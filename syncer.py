fac640ece5fbba6e5c286723ded2a49d1cf3feb3
Daniel Chen <daniel.n.chen@gmail.com>
2011-11-01 09:16:16 +0000

    got state syncing working

--- syncer.py
+++ syncer.py
@@ -13,34 +13,28 @@ def process_file(fn):
 
 
 if __name__ == '__main__':
-    # dict of cur -> next
     changes = {}
-    timestamps = {}
-    filenames = {}
     for fn in os.listdir(syncdir):
         m = re.match(changes_re, fn)
         if m:
-            ts = m.group('ts')
             cur = m.group('cur')
-            next = m.group('next')
-            changes[cur] = next
-            timestamps[cur] = ts
-            filenames[cur] = fn
+            changes[cur] = m.groupdict()
+            changes[cur].update(fn=fn)
 
     with open(state) as processed:
         processed_so_far = processed.readlines()
         if len(processed_so_far) == 0:
-            for fn in filenames.values():
-                if fn.startswith('00000000000000'):
-                    m = re.match(changes_re, fn)
-                    next_hash = m.group('cur')
-                    break
+			for change in changes:
+				if change['fn'].startswith('00000000000000'):
+					next_hash = change['next']
+					break
         else:
             next_hash = processed_so_far[-1].rstrip('\n')
 
     with open(state, 'a') as processed:
+        next_hash = changes[next_hash]['next']
         while next_hash in changes:
-            process_file(filenames[next_hash])
-            #processed.write(next_hash+'\n')
-            next_hash = changes[next_hash]
+            process_file(changes[next_hash]['fn'])
+            processed.write(next_hash+'\n')
+            next_hash = changes[next_hash]['next']
         
